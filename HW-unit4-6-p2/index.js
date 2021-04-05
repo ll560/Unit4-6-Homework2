3139c0ddb667e7118f4400014a840f6615dff206
ll560 <llavia6@gmail.com>
2021-04-05 12:20:14 -0400

    first commit

--- HW-unit4-6-p2/index.js
+++ HW-unit4-6-p2/index.js
@@ -2,30 +2,52 @@
 
 //Using what you did with Objects perdiod 2 from the slides, create a personal profile. Instead of Products you will need to create a personal profile. Initialize Constructors and More!
 //Step 1
-
+function Profile(name, monthOfBirth, hobbies, location){
+    this.monthOfBirth = monthOfBirth;
+    this.name = name;
+    this.hobbies = hobbies;
+    this.location = location;
+}
 
 
 
 //Step 2
-
+//create object
+let personalProfile = new Profile('Lisa', 2, 'coding')
 
 
 
 //Step 3
+let personalProfile1 = new Profile('John', 3, 'running')
 
+let personalProfile2 = new Profile('Kim', 4, 'reading')
 
+let personalProfile3 = new Profile('Sam', 5, 'biking')
 
 //Step 4
+function Lives(cityName, location){ 
+    this.cityName = cityName;
+    this.location = location;
 
+}
+
+//add new objects lives1-2-3
+let lives1 = new Friend('Boston', false);
+let lives2 = new Friend('Cambridge', true);
+let lives3 = new Friend('Quincy', false);
+console.log(lives1, lives2, lives3);
 
 //Step 5
 
 
 //Step 6
+let completeProfile1 = new Profile('John', 3, 'running', lives1)
 
+let completeProfile2 = new Profile('Kim', 4, 'reading', lives2)
 
+let completeProfile3 = new Profile('Sam', 5, 'biking', lives3)
 // Step 7
-
+completeProfile3.favoriteFood = "apples";
 
 // Step 8
 
@@ -33,13 +55,23 @@
 //.Create
 
 //Step 1
+let myProfile = {
+    type: 'Lisa',
+    displyType: function(){
+        console.log(this.type);
+    }
+};
 
+myProfile.displayType();
 
 //Step 2
-
+let PersonalProfile1 = Object.create(myProfile);
+PersonalProfile1.displayType();
 
 //Step 3
-
+let ownsPet = Object.create(PersonalProfile1);
+ownsPet.type = 'Dog';
+ownsPet.displayType();
 
 
 //Array vs Objects
